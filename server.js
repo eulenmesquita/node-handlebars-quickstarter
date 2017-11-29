519e403137e379b5afe46ef74e6901b9027b1f29
eulen@minicode.com.au <eulen@minicode.com.au>
2017-11-30 09:14:25 +1000

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express=require('express');
+var path = require('path');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.set('views', path.join(__dirname,'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.set('port', (process.env.PORT || 3000));
+
+app.get('/', function(req,res) {
+    res.render('home');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('server started on port '+app.get('port'));
+});
